2db107398c1260427c260a21a4cbcb4997d0878b
Elizaveta Khaletskaya <effy4814@gmail.com>
2023-06-02 15:22:19 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main (String [] args) {
+
+        int eaters = 5;
+
+        int water = 3000;
+        int potatoes = 5;
+        int chicken = 6;
+        int spices = 10;
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров (а) воды ");
+        System.out.println((potatoes / eaters) + " картофелин (а/ы) ");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(о) ");
+        System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+
+    }
 }
